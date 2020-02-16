eb63bde35c3b6d7ae380458e84b124e10a0b9ead
Nguyen Minh Khanh <edgpawn1999@gmail.com>
2020-02-16 19:39:36 +0700

    Sửa lần thứ 6
    Tôi đã tìm ra 1 chân lí mới, nhưng thiết kế của tôi thì như lồn

--- App mua hang/MainAppForAdmin.Designer.cs
+++ App mua hang/MainAppForAdmin.Designer.cs
@@ -28,106 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainAppForAdmin));
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.viewMenu = new System.Windows.Forms.Button();
-            this.chatWC = new System.Windows.Forms.PictureBox();
-            this.cusRecharge = new System.Windows.Forms.PictureBox();
-            this.addAccount = new System.Windows.Forms.PictureBox();
-            this.addMore = new System.Windows.Forms.PictureBox();
-            this.panelSlide = new System.Windows.Forms.Panel();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buttonDB = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chatWC)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.cusRecharge)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.addAccount)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.addMore)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(-3, 1);
+            this.panel1.Location = new System.Drawing.Point(247, 1);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1131, 57);
+            this.panel1.Size = new System.Drawing.Size(881, 57);
             this.panel1.TabIndex = 0;
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.viewMenu);
-            this.panel2.Controls.Add(this.chatWC);
-            this.panel2.Controls.Add(this.cusRecharge);
-            this.panel2.Controls.Add(this.addAccount);
-            this.panel2.Controls.Add(this.addMore);
-            this.panel2.Location = new System.Drawing.Point(-3, 57);
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(-3, -8);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(75, 597);
+            this.panel2.Size = new System.Drawing.Size(252, 665);
             this.panel2.TabIndex = 1;
             // 
-            // viewMenu
+            // pictureBox1
             // 
-            this.viewMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.viewMenu.Image = ((System.Drawing.Image)(resources.GetObject("viewMenu.Image")));
-            this.viewMenu.Location = new System.Drawing.Point(4, 0);
-            this.viewMenu.Name = "viewMenu";
-            this.viewMenu.Size = new System.Drawing.Size(71, 60);
-            this.viewMenu.TabIndex = 3;
-            this.viewMenu.UseVisualStyleBackColor = true;
-            this.viewMenu.Click += new System.EventHandler(this.viewMenu_Click);
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(53, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 141);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
-            // chatWC
+            // panel3
             // 
-            this.chatWC.Image = ((System.Drawing.Image)(resources.GetObject("chatWC.Image")));
-            this.chatWC.Location = new System.Drawing.Point(4, 450);
-            this.chatWC.Name = "chatWC";
-            this.chatWC.Size = new System.Drawing.Size(72, 65);
-            this.chatWC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.chatWC.TabIndex = 5;
-            this.chatWC.TabStop = false;
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.buttonDB);
+            this.panel3.Location = new System.Drawing.Point(2, 219);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(252, 431);
+            this.panel3.TabIndex = 2;
             // 
-            // cusRecharge
+            // buttonDB
             // 
-            this.cusRecharge.Image = ((System.Drawing.Image)(resources.GetObject("cusRecharge.Image")));
-            this.cusRecharge.Location = new System.Drawing.Point(4, 330);
-            this.cusRecharge.Name = "cusRecharge";
-            this.cusRecharge.Size = new System.Drawing.Size(72, 65);
-            this.cusRecharge.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.cusRecharge.TabIndex = 4;
-            this.cusRecharge.TabStop = false;
+            this.buttonDB.FlatAppearance.BorderSize = 0;
+            this.buttonDB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDB.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDB.Image = ((System.Drawing.Image)(resources.GetObject("buttonDB.Image")));
+            this.buttonDB.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonDB.Location = new System.Drawing.Point(-1, -1);
+            this.buttonDB.Name = "buttonDB";
+            this.buttonDB.Size = new System.Drawing.Size(250, 90);
+            this.buttonDB.TabIndex = 0;
+            this.buttonDB.Text = "Dashboard";
+            this.buttonDB.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonDB.UseVisualStyleBackColor = true;
             // 
-            // addAccount
+            // button2
             // 
-            this.addAccount.Image = ((System.Drawing.Image)(resources.GetObject("addAccount.Image")));
-            this.addAccount.Location = new System.Drawing.Point(4, 217);
-            this.addAccount.Name = "addAccount";
-            this.addAccount.Size = new System.Drawing.Size(72, 65);
-            this.addAccount.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.addAccount.TabIndex = 4;
-            this.addAccount.TabStop = false;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(0, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 90);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // addMore
+            // button3
             // 
-            this.addMore.Image = ((System.Drawing.Image)(resources.GetObject("addMore.Image")));
-            this.addMore.Location = new System.Drawing.Point(4, 103);
-            this.addMore.Name = "addMore";
-            this.addMore.Size = new System.Drawing.Size(72, 65);
-            this.addMore.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.addMore.TabIndex = 4;
-            this.addMore.TabStop = false;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(0, 267);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 90);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
-            // panelSlide
+            // button4
             // 
-            this.panelSlide.BackColor = System.Drawing.Color.DimGray;
-            this.panelSlide.Location = new System.Drawing.Point(71, 57);
-            this.panelSlide.Name = "panelSlide";
-            this.panelSlide.Size = new System.Drawing.Size(379, 597);
-            this.panelSlide.TabIndex = 2;
-            // 
-            // timer1
-            // 
-            this.timer1.Interval = 30;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(0, 178);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(250, 90);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // MainAppForAdmin
             // 
@@ -135,16 +135,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(25)))), ((int)(((byte)(30)))));
             this.ClientSize = new System.Drawing.Size(1128, 654);
-            this.Controls.Add(this.panelSlide);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "MainAppForAdmin";
             this.Text = "MainAppForAdmin";
             this.panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.chatWC)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.cusRecharge)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.addAccount)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.addMore)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -153,12 +150,11 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panelSlide;
-        private System.Windows.Forms.PictureBox chatWC;
-        private System.Windows.Forms.PictureBox cusRecharge;
-        private System.Windows.Forms.PictureBox addAccount;
-        private System.Windows.Forms.PictureBox addMore;
-        private System.Windows.Forms.Button viewMenu;
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonDB;
     }
 }
--- App mua hang/MainAppForAdmin.cs
+++ App mua hang/MainAppForAdmin.cs
@@ -12,42 +12,12 @@ namespace App_mua_hang
 {
     public partial class MainAppForAdmin : Form
     {
-        int panelWidth;
-        bool Hidden;
 
         public MainAppForAdmin()
         {
             InitializeComponent();
-            panelWidth = viewMenu.Width;
-            Hidden = false;
         }
 
-        private void timer1_Tick_1(object sender, EventArgs e)
-        {
-            if (Hidden)
-            {
-                panelSlide.Width = panelSlide.Width + 10;
-                if (panelSlide.Width >= panelWidth)
-                {
-                    timer1.Stop();
-                    Hidden = false;
-                    this.Refresh();
-                }
-            }
-            else
-            {
-                panelSlide.Width = panelSlide.Width - 10;
-                if (panelSlide.Width < 0)
-                {
-                    timer1.Stop();                  Hidden = true;
-                    this.Refresh();
-                }
-            }
-        }
-
-        private void viewMenu_Click(object sender, EventArgs e)
-        {
-            timer1.Start();
-        }
+       
     }
 }
